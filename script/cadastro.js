a8aff21c7290a5f5d58f7e3de46a574df0a69498
Samuel.Costa <samuel.costa@forlogic.net>
2025-06-05 14:42:40 -0300

    #96266

--- script/cadastro.js
+++ script/cadastro.js
@@ -13,59 +13,83 @@ function validaUsuario(){
         window.location.href = "../pages/login.html"
     }
 };
+if(document.getElementById("tabela-cadastros")){
+    function preencherTabela(){
 
-function preencherTabela(){
+        const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+        for(var i = usuarios.length - 1; i >= 0 ; i--){
+            const usuario = usuarios[i];
+            if(usuario.status == "Ativo"){
+            var tabela = document.getElementById("tabela-cadastros")
+            var qtdLinhas = tabela.rows.length;
+            var linha = tabela.insertRow(qtdLinhas)
 
-    const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
-    for(var i = usuarios.length - 1; i >= 0 ; i--){
-        const usuario = usuarios[i];
-        if(usuario.status == "Ativo"){
-        var tabela = document.getElementById("tabela-cadastros")
-        var qtdLinhas = tabela.rows.length;
-        var linha = tabela.insertRow(qtdLinhas)
+            var celulaNome = linha.insertCell(0)
+            var celulaEmail = linha.insertCell(1)
+            var celulaStatus = linha.insertCell(2)
 
-        var celulaNome = linha.insertCell(0)
-        var celulaEmail = linha.insertCell(1)
-        var celulaStatus = linha.insertCell(2)
+            var link = '<a class="link-usuario" id="'+ i +'" href="#">'+ usuario.nome +'</a>';     
 
-        var link = '<a class="link-usuario" id="'+ i +'" href="#">'+ usuario.nome +'</a>';     
-
-        celulaNome.innerHTML = link
-        celulaEmail.innerText = usuario.email
-        celulaStatus.innerText = usuario.status
+            celulaNome.innerHTML = link
+            celulaEmail.innerText = usuario.email
+            celulaStatus.innerText = usuario.status
+            }
         }
-    }
-    for(var i = usuarios.length - 1; i >= 0 ; i--){
-        const usuario = usuarios[i];
-        if(usuario.status == "Inativo"){
-        var tabela = document.getElementById("tabela-cadastros")
-        var qtdLinhas = tabela.rows.length;
-        var linha = tabela.insertRow(qtdLinhas)
+        for(var i = usuarios.length - 1; i >= 0 ; i--){
+            const usuario = usuarios[i];
+            if(usuario.status == "Inativo"){
+            var tabela = document.getElementById("tabela-cadastros")
+            var qtdLinhas = tabela.rows.length;
+            var linha = tabela.insertRow(qtdLinhas)
 
-        var celulaNome = linha.insertCell(0)
-        var celulaEmail = linha.insertCell(1)
-        var celulaStatus = linha.insertCell(2)
+            var celulaNome = linha.insertCell(0)
+            var celulaEmail = linha.insertCell(1)
+            var celulaStatus = linha.insertCell(2)
 
-        var link = '<a class="link-usuario" id="'+ i +'" href="#">'+ usuario.nome +'</a>';    
+            var link = '<a class="link-usuario" id="'+ i +'" href="#">'+ usuario.nome +'</a>';    
 
-        celulaNome.innerHTML = link
-        celulaEmail.innerText = usuario.email
-        celulaStatus.innerText = usuario.status
+            celulaNome.innerHTML = link
+            celulaEmail.innerText = usuario.email
+            celulaStatus.innerText = usuario.status
 
-        linha.style.color = 'gray'
+            linha.style.color = 'gray'
+            }
         }
-    }
-    const links = document.querySelectorAll('.link-usuario');
-    links.forEach(link => {
-        link.addEventListener('click', (event) => {
-            const elementoClicado = event.target;
-            const idLink = elementoClicado.id;
-            const usuario = usuarios[idLink];
-            localStorage.setItem("usuario", JSON.stringify(usuario))
-            window.location.href = "../pages/cadastro.html"
+        const links = document.querySelectorAll('.link-usuario');
+        links.forEach(link => {
+            link.addEventListener('click', (event) => {
+                const elementoClicado = event.target;
+                const idLink = elementoClicado.id;
+                const usuario = usuarios[idLink];
+                localStorage.setItem("usuario", JSON.stringify(usuario))
+                const modal = document.querySelector("dialog")
+                modal.showModal()
+            });
         });
-    });
+}
 }
 
-preencherTabela()
+
+const modal = document.querySelector("dialog")
+const botaoModal = document.getElementById("novo-cadastro")
+const botaoClose = document.getElementById("btn-cancelar")
+
+botaoModal.onclick = function (){
+    modal.showModal()
+}
+botaoClose.onclick = function(){
+    modal.close()
+    localStorage.removeItem("usuario")
+}
+
+function editarUsuario(){
+    const usuario = JSON.parse(localStorage.getItem("usuario"));
+    const modal = document.querySelector("dialog")
+    if(usuario){
+        modal.showModal()
+    }
+}
+
+preencherTabela();
 validaUsuario();
+editarUsuario();
